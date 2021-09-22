4a19bf632cf8c3eb8773db6588c6638be1ef72ce
Your NameNisarga1999r <you@example.comravikumar34778@gmail.com>
2021-09-23 00:09:37 +0530

    write negation operaters program

---
+++ NegationOperation.java
@@ -0,0 +1,15 @@
+class NegationOperation{
+public static void main(String[] args){
+boolean b = true;
+boolean result = !b;
+System.out.println("b: "+b);
+System.out.println("result: "+result);
+
+boolean c = false;
+boolean res = !c;
+System.out.println("c: "+c);
+System.out.println("res: "+res);
+
+}
+}
+
