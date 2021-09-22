a5d4c2bdc23fb3df5d7a045aa52ae7ebb5e25ea8
Your NameNisarga1999r <you@example.comravikumar34778@gmail.com>
2021-09-23 00:03:14 +0530

    write increment operaters program

---
+++ IncrementOperator.java
@@ -0,0 +1,14 @@
+class IncrementOperator{
+public static void main(String[] args){
+int a = 11;
+int b = ++a;
+System.out.println("a: "+a);
+System.out.println("b: "+b);
+
+int c = 25;
+int d = c++;
+System.out.println("c: "+c);
+System.out.println("d: "+d);
+
+}
+}
