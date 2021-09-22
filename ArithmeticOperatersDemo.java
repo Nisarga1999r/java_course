8182a1abd1c2919f1babe0f3f24f8c3004cadcb1
Your NameNisarga1999r <you@example.comravikumar34778@gmail.com>
2021-09-22 22:03:43 +0530

    write arithmetic operaters program

---
+++ ArithmeticOperatersDemo.java
@@ -0,0 +1,30 @@
+class ArithmeticOperatersDemo{
+public static void main(String[] args){
+int a = 10;
+int b = 20;
+int addResult = a+b;
+System.out.println("addResult : "+addResult);
+int c = 100;
+int d = 200;
+int subtractResult = d-c;
+System.out.println("subtractResult : "+subtractResult);
+int e = 25;
+int f = 12;
+int mulresult = e*f;
+System.out.println("mulresult : "+mulresult);
+int g = 100;
+int h = 25;
+int divresult = g/h;
+System.out.println("divresult : "+divresult);
+int i = 10;
+int j = 2;
+int modresult = i%j ;
+System.out.println("modresult : "+modresult);
+int k = 4;
+int l = 30;
+int modulusresult = l%k;
+System.out.println("modulusresult : "+modulusresult);
+
+
+}
+}
