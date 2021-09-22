0042b23ca4494cab4daa3a905bc88460cd28b39b
Your NameNisarga1999r <you@example.comravikumar34778@gmail.com>
2021-09-23 00:06:32 +0530

    write decrement operaters program

---
+++ DecrementOperater.java
@@ -0,0 +1,13 @@
+class DecrementOperater{
+public static void main(String[] args){
+int y = 98;
+int z = --y;
+System.out.println("y: "+y);
+System.out.println("z: "+z);
+
+int w = 99;
+int x = w--;
+System.out.println("w: "+w);
+System.out.println("x: "+x);
+}
+}
