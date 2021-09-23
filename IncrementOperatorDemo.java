05b69f86cb8165fd842d4c92912467cc067bba6d
Your NameNisarga1999r <you@example.comravikumar34778@gmail.com>
2021-09-23 08:24:06 +0530

    write increment program

---
+++ IncrementOperatorDemo.java
@@ -0,0 +1,16 @@
+class IncrementOperatorDemo{
+public static void main(String[] args){
+System.out.println("pre increment operation----------");
+int a = 1;
+int b = ++a;
+System.out.println("a: "+a);
+System.out.println("b: "+b);
+
+System.out.println("post increment operation----------");
+int i = 1;
+int j = i++;
+System.out.println("i: "+i);
+System.out.println("j: "+j);
+
+}
+}
