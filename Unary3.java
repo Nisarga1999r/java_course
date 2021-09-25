e2ad81a20eafdad1e194ada08f8807921513eb7a
Your NameNisarga1999r <you@example.comravikumar34778@gmail.com>
2021-09-25 23:59:06 +0530

    writes program on unary operator

---
+++ Unary3.java
@@ -0,0 +1,14 @@
+class Unary3   
+{  
+public static void main(String args[])  
+{  
+int i = 10;  
+i++;   
+System.out.println(i);  
+++i;                  
+System.out.println(i);  
+System.out.println(++i);   
+System.out.println(i++);   
+System.out.println(i);  
+}  
+}  
