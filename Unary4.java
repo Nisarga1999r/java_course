58b49b982776763717084e5778a1c5434d121d94
Your NameNisarga1999r <you@example.comravikumar34778@gmail.com>
2021-09-25 23:59:29 +0530

    writes program on unary operator

---
+++ Unary4.java
@@ -0,0 +1,15 @@
+class Unary4     
+{  
+public static void main(String args[])  
+{  
+int i = 128;  
+i--;  
+System.out.println(i);  
+--i;                   
+System.out.println(i);    
+System.out.println(--i);    
+System.out.println(i--);    
+System.out.println(i--);  
+System.out.println(i);
+}  
+}  
