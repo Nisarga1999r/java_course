44e357591ff56d952530b1257c5e5cfec37e3944
Your NameNisarga1999r <you@example.comravikumar34778@gmail.com>
2021-09-27 00:00:42 +0530

    writes program on unary assignment operator

---
+++ AssignmentOperatorDemo.java
@@ -0,0 +1,16 @@
+class AssignmentOperatorDemo{
+public static void main(String[] args){
+int m=50;
+int n=10;
+n=n+m;
+System.out.println("n:"+n);
+System.out.println();
+
+int a=501;
+int b=10;
+b+=a;
+System.out.println("b:"+b);
+
+
+}
+}
