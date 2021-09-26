93bfcd45a3b4770f69a9a726cd3076337f86a934
Your NameNisarga1999r <you@example.comravikumar34778@gmail.com>
2021-09-26 21:25:53 +0530

    writes unary program

---
+++ Unary3a.java
@@ -0,0 +1,34 @@
+class Unary3a   
+{  
+public static void main(String args[])  
+{  
+int i = 10; 
+int x=10;  
+i++;   
+System.out.println(i);  
+++i;                  
+System.out.println(i);  
+System.out.println(++i);   
+System.out.println(i++);   
+System.out.println(i); 
+ 
+System.out.println();
+
+System.out.println(x++); 
+System.out.println(++x); 
+System.out.println(x--); 
+System.out.println(--x);
+
+System.out.println();
+
+int n = 14;
+System.out.println(i);
+i++;
+i++;
+i--;
+System.out.println(i);
+System.out.println(--i);
+
+
+}  
+}  
