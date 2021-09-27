16d388accebbf6e5ab564288fc1b2cf7f119e5a4
Your NameNisarga1999r <you@example.comravikumar34778@gmail.com>
2021-09-27 09:03:37 +0530

    writes condition program

---
+++ ConditionDemo.java
@@ -0,0 +1,23 @@
+class ConditionDemo{
+public static void main(String[] args)
+{
+int a = 20;
+int b = 30;
+if (a>b){
+System.out.println("a is largest");
+}else{
+System.out.println("b is largest");
+}
+
+System.out.println();
+
+int c = 20;
+int d = 30;
+if (c<d){
+System.out.println("c is largest");
+}else{
+System.out.println("d is largest");
+}
+}
+}
+
