109aa33e15023106fc1a1936e5e2940577c6fea4
Your NameNisarga1999r <you@example.comravikumar34778@gmail.com>
2021-09-27 12:30:53 +0530

    writes ternary program

---
+++ TernaryOperator3.java
@@ -0,0 +1,15 @@
+class TernaryOperator3{
+public static void main(String[] args){
+int a = 12;
+int b = 13;
+int c = 18;
+int d;
+int e;
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+d = a<b?a:b;
+e = c<d?c:d;
+System.out.println("The smallest number is: "+e);
+}
+}
