f0e5cd12b70c730ac7cd7258d77e1a560e1ffb66
Your NameNisarga1999r <you@example.comravikumar34778@gmail.com>
2021-09-27 23:33:44 +0530

    writes operator shifting program

---
+++ OperatorShifting2.java
@@ -0,0 +1,9 @@
+class OperatorShifting2 {
+    public static void main(String args[])  {       
+     int x = -1;  
+  
+       System.out.println(x>>>29);  
+       System.out.println(x>>>30);  
+       System.out.println(x>>>31);   
+   }    
+} 
