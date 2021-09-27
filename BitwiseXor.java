4d5a938a305e2208f510922543923c4f40b70f4c
Your NameNisarga1999r <you@example.comravikumar34778@gmail.com>
2021-09-27 08:09:55 +0530

    pgm

---
+++ BitwiseXor.java
@@ -0,0 +1,12 @@
+class BitwiseXor{
+public static void main(String[] args)
+{
+int x = 4;
+int y = 8;
+int z = 2;
+System.out.println("x^y="+(x^y));
+System.out.println("y^x="+(y^x));
+System.out.println("x^z="+(x^z));
+}
+}
+
