450592987dd0bd1ca06a30323bb712165c67b561
Your NameNisarga1999r <you@example.comravikumar34778@gmail.com>
2021-09-27 12:05:13 +0530

    writes ternary program

---
+++ TernaryOperator1.java
@@ -0,0 +1,11 @@
+class TernaryOperator1{
+public static void main(String[] args){
+int x = 100;
+int y = 200;
+int z = 250;
+int result = x>y ? x:y;
+
+System.out.println("largest of " +x+" & "+y+ "is: "+result);
+
+}
+}
