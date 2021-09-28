940681a1795d4a8e40a0d6bcdea5640a157bc277
Your NameNisarga1999r <you@example.comravikumar34778@gmail.com>
2021-09-28 23:36:04 +0530

    writes novel object creation program

---
+++ Novel.java
@@ -0,0 +1,25 @@
+class Novel{
+String name;
+String color;
+
+int noOfPages;
+ void knowledge()
+  {
+    System.out.println(color+" color "+name+" book gives you knowledge");
+ }
+  
+public static void main(String[] args){
+  Novel n1=new Novel();
+  n1.name="The Life";
+  n1.color="Black";
+ 
+  n1.noOfPages=2;
+  n1.knowledge();
+  
+ }
+}
+
+
+
+  
+
