569cf8a6e78813b69480fa41700970125e5795eb
Your NameNisarga1999r <you@example.comravikumar34778@gmail.com>
2021-09-28 11:46:44 +0530

    writes logicalxor operator program

---
+++ LogicalXor.java
@@ -0,0 +1,14 @@
+class LogicalXor{
+public static void main(String[] args)
+{
+int x = 4;
+int y = 8;
+int z = 2;
+System.out.println((5==5)^(5==6));
+System.out.println((5==5)^(6==6));
+System.out.println((4==5)^(5==6));
+System.out.println((5==5)^(6==6));
+System.out.println((5==5)^(5==6)^(6==7)^(7==8));
+}
+}
+
