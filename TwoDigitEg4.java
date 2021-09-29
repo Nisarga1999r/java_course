2829f8103a7cff463fef49dbd14a3d11fa43852b
Your NameNisarga1999r <you@example.comravikumar34778@gmail.com>
2021-09-29 23:40:43 +0530

    writes program

---
+++ TwoDigitEg4.java
@@ -0,0 +1,16 @@
+class TwoDigitEg4
+{
+public static void main(String args[]){
+    int num=22;
+int num=2;
+    
+       if(num >= 10 && num < 100){
+            System.out.println("it is two digit number");
+        }else
+        {
+           System.out.println("it is Not Two-Digit Number");
+            }
+        }
+    }
+     
+    
