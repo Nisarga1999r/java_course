a27c56625c1719e2b63b71d030a846e0793875b2
Your NameNisarga1999r <you@example.comravikumar34778@gmail.com>
2021-09-29 23:52:51 +0530

    writes program

---
+++ TwoDigitEg8.java
@@ -0,0 +1,14 @@
+class TwoDigitEg8
+{
+public static void main(String args[]){
+    int num=121235;
+
+    
+       if(num >= 10 && num < 100){
+            System.out.println("it is two digit number");
+        }
+       
+        }
+    }
+     
+    
