02bd2c26af34af6c1a8ca86a1b4c88ab4d735eae
Your NameNisarga1999r <you@example.comravikumar34778@gmail.com>
2021-09-29 21:36:05 +0530

    writes program

---
+++ TwoDigitEg2.java
@@ -0,0 +1,31 @@
+class TwoDigitEg2
+{
+    static void specialNumber(int n)
+    {
+       if(n < 10 || n > 99)
+            System.out.println("Invalid Input! " +
+                           "Number should have " +
+                                 "2 digits only");
+        else
+        {
+            int first = n / 10;
+            int last = n % 10;
+            int sum = first + last;
+            int pro = first * last;
+             
+            if((sum + pro) == n)
+            {
+              System.out.println(n + " is Two-Digit Number");
+            }
+            else
+            {
+              System.out.println(n +" is Not Two-Digit Number");
+            }
+        }
+    }
+     public static void main(String args[])
+    {
+        int n = 691;
+        specialNumber(n);
+    }
+}
