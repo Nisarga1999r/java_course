206f570f25811713666067536e25e3aed444183a
Your NameNisarga1999r <you@example.comravikumar34778@gmail.com>
2021-10-01 00:16:42 +0530

    writes program

---
+++ IfElseIf2.java
@@ -0,0 +1,29 @@
+class IfElseIf21 {
+      public static void main(String args[]){
+	int num=21;
+	if(num ==1) {
+	  System.out.println("monday");
+	}
+	else if(num ==2) {
+	  System.out.println("sunday");
+	}
+	else if(num ==3) {
+	  System.out.println("saturday");
+	}
+	else if(num ==4) {
+	  System.out.println("friday");			
+	}
+        else if(num ==5) {
+	  System.out.println("thursday");
+       }
+       else if(num ==6) {
+	  System.out.println("wednesday");
+       }
+       else if(num ==7) {
+	  System.out.println("tuesday");
+       }
+       else  {
+	  System.out.println("please enter number between 1 to 7");
+       }
+      }
+    }
