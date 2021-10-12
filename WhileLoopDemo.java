42b9837ce9e256408d5e064081a0824b31839da6
Your NameNisarga1999r <you@example.comravikumar34778@gmail.com>
2021-10-12 23:41:31 +0530

    pgm

---
+++ WhileLoopDemo.java
@@ -0,0 +1,13 @@
+class WhileLoopDemo{
+public static void main(String[] args){
+
+System.out.println("main started");
+int age=18;
+while(age==18)
+{
+System.out.println("your age is 18");
+age++;
+}
+System.out.println("main ended");
+}
+}
