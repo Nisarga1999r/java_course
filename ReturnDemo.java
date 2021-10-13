8cd3d94203ca1e19bd8a2fb8cbe36672d38355fd
Your NameNisarga1999r <you@example.comravikumar34778@gmail.com>
2021-10-13 23:48:56 +0530

    pgm

---
+++ ReturnDemo.java
@@ -0,0 +1,12 @@
+class ReturnDemo{
+static int squareRoot(int number){
+int squar=number*number;
+
+
+return squar;
+}
+public static void main(String[] args){
+int square=squareRoot(20);
+System.out.println("square :"+square);
+}
+}
