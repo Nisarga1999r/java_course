206a262e31103cf83aa83e3f4605a0892889e09f
Your NameNisarga1999r <you@example.comravikumar34778@gmail.com>
2021-10-20 08:03:12 +0530

    pgm

---
+++ MethodOverloadingA.java
@@ -0,0 +1,29 @@
+public class MethodOverloadingA {
+	
+	void sum(int a, int b)
+	{
+		System.out.println("a+b: "+(a+b));
+	}
+	
+	void sum(int x, int y,int z)
+	{
+		System.out.println("x+y+z: "+(x+y+z));
+	}
+	 
+	void sum(float m, float n)
+	{
+		System.out.println("m+n: "+(m+n));
+	}
+
+	public static void main(String[] args) {
+		
+      MethodOverloading mod=new MethodOverloading();
+      
+      mod.sum(1,3);
+      mod.sum(1,2,3);
+      mod.sum(1.1f,2.2f);
+	}
+
+	
+		
+	}
