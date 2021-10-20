56b22cd2f1c1407886610968d29c941da70d6fc6
Nisarga1999r <72214804+Nisarga1999r@users.noreply.github.com>
2021-10-21 00:56:58 +0530

    MethodOverloading program

--- MethodOverloading.java
+++ MethodOverloading.java
@@ -1,18 +1,26 @@
+
 public class MethodOverloading {
-	void sum(int a, int b) {
-		System.out.println("a+b:"+(a+b));
-	}
-	void sum(int q, int d,int e)
+	void sum(int a, int b)
 	{
-		System.out.println("q+d+e"+(q+d+e));
+		System.out.println("a+b: "+(a+b));
 	}
-	void sum(float p,float n) {
-		System.out.println("p+n:"+(p+n));
+	
+	void sum(int x, int y,int z)
+	{
+		System.out.println("x+y+z: "+(x+y+z));
 	}
-public static void main(String[] args) {
-	MethodOverloading mod=new MethodOverloading();
-	mod.sum(1.1f, 2.2f);
-	mod.sum(1,2);
-	mod.sum(1,2,3);
+	 
+	void sum(float m, float n)
+	{
+		System.out.println("m+n: "+(m+n));
 	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MethodOverloading mod=new MethodOverloading();
+	      
+	      mod.sum(1,3);
+	      mod.sum(1,2,3);
+	      mod.sum(1.1f,2.2f);
+	}
+
 }
