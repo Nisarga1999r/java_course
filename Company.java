1d5f2b50662cdf4a4604b4d793bb43beff57635d
Nisarga1999r <72214804+Nisarga1999r@users.noreply.github.com>
2021-10-25 00:23:00 +0530

    Add files via upload
    Constructors

---
+++ Company.java
@@ -0,0 +1,11 @@
+package constructors;
+
+public class Company {
+	String name;
+	String place;
+
+	Company()
+	{
+		System.out.println("X WORK Z.....");
+	}
+}
---
+++ Doll.java
@@ -0,0 +1,10 @@
+package constructors;
+
+public class Doll {
+	String name;
+    double price;
+
+    Doll(){
+    	System.out.println("CONSTRUCTOR CALLED....");
+    }
+}
